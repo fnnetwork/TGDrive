3a5a94c72eefbf5cfb352e65aa976c3dcbd8a8ba
fnnetwork <fnnetwork272@gmail.com>
2025-03-11 20:39:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -356,6 +356,5 @@ async def getFolderShareAuth(request: Request):
         return JSONResponse({"status": "not found"})
 
 
-if __name__ == '__main__':
-    # Set host to '0.0.0.0' to make the server accessible on the local network
-    app.run(host='0.0.0.0', port=10000, debug=True)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=10000, reload=True)
